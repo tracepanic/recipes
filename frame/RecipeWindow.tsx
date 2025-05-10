f1249693d44b58e47b51bba21c4198464fa6d732
Anuj Gupta <anujgupta4155@gmail.com>
2025-05-11 00:05:15 +0530

    Add form validation

--- frame/RecipeWindow.tsx
+++ frame/RecipeWindow.tsx
@@ -11,6 +11,7 @@ export default function Recipes_Window({ id, children }: CategoryWindowProps) {
     const [openModel, setOpenModel] = useState<boolean>(false);
     const [ingredient, setIngredient] = useState<string>("");
     const [ingredients, setIngredients] = useState<Array<string>>([""]);
+    const [emoji, setEmoji] = useState<string>("");
     const handleDragOver = (e: any) => {
         e.preventDefault();
     };
@@ -37,27 +38,39 @@ export default function Recipes_Window({ id, children }: CategoryWindowProps) {
                 {children}
             </div>
             {openModel && (
-                <div className="absolute  gap-y-2 flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white/10 z-10 p-12 rounded-xl backdrop-blur-3xl border-blue-300 shadow-2xl shadow-cyan-500/50 border-solid border-[0.2px] ">
-                    <input
-                        className="box-border  tracking-wider border-2 border-gray-800/50 p-2  focus:border-blue-500 rounded outline-0"
-                        type="text"
-                        placeholder="Recipe Name"
-                    />
-                    <input
-                        className="box-border tracking-wider border-2 border-gray-800/50 p-2 focus:border-blue-500 rounded outline-0"
-                        type="text"
-                        placeholder="Good / Bad / Average"
-                    />
-                    <div className="flex border-[0.8px] overflow-scroll flex-wrap border-gray-800/50 p-1 rounded  gap-1 min-h-16">
-                        {ingredients.map((ingredient, index) => {
-                            if (ingredient !== "") {
-                                return (
+                <div className="absolute  gap-y-2 flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white/10 z-1000 p-12 rounded-xl backdrop-blur-3xl border-blue-300 shadow-2xl shadow-cyan-500/50 border-solid border-[0.2px] ">
+                    <form className="flex flex-col gap-y-2">
+                        <input
+                            className="box-border  tracking-wider border-2 border-gray-800/50 p-2  focus:border-blue-300 rounded outline-0"
+                            type="text"
+                            name="recipe"
+                            pattern="(?:[a-zA-Z].*?[a-zA-Z])"
+                            title="Enter at least two character"
+                            placeholder="Recipe Name"
+                            required={true}
+                        />
+                        <input
+                            name="emoji"
+                            className="box-border tracking-wider border-2 border-gray-800/50 p-2 focus:border-blue-300 rounded outline-0"
+                            type="text"
+                            placeholder="Good / Bad / Average"
+                            pattern="^(good|bad|average)$"
+                            required
+                            onInput={(e) => {
+                                const target = e.target as HTMLInputElement;
+                                target.value = target.value.toLowerCase();
+                            }}
+                            title="Enter bad or good or average"
+                        />
+                        <div className="flex border-[0.8px]  flex-wrap border-gray-800/50 p-1 rounded  gap-1 min-h-16">
+                            {ingredients.map((ingredient, index) =>
+                                ingredient ? (
                                     <div
                                         className="rounded flex justify-center items-center  border-[0.8px] h-fit  border-gray-800/50 p-1"
                                         key={index}
                                     >
                                         {ingredient}
-                                        <button
+                                        <div
                                             className="m-1"
                                             onClick={() =>
                                                 setIngredients(() =>
@@ -85,60 +98,69 @@ export default function Recipes_Window({ id, children }: CategoryWindowProps) {
                                                     d="M15.54 8.46L13.41 10.59L15.54 12.71L14.12 14.12L12 12L9.88 14.12L8.46 12.71L10.59 10.59L8.46 8.46L9.88 7.05L12 9.17L14.12 7.05L15.54 8.46Z"
                                                 />
                                             </svg>
-                                        </button>
+                                        </div>
                                     </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="flex rounded pr-2 border-2  border-gray-600/50 focus-within:border-blue-500">
-                        <input
-                            onChange={(
-                                e: React.ChangeEvent<HTMLInputElement>
-                            ) => setIngredient(e.target.value)}
-                            className="box-border p-2 tracking-wider  rounded outline-0"
-                            type="text"
-                            placeholder="Ingredients"
-                        />
-                        <button
-                            className="cursor-pointer"
-                            onClick={() =>
-                                setIngredients([...ingredients, ingredient])
-                            }
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="32"
-                                height="32"
-                                viewBox="0 0 24 24"
-                                className="hover:fill-blue-500 fill-gray-500"
+                                ) : null
+                            )}
+                        </div>
+                        <div className="flex rounded pr-2 border-2 items-center border-gray-800/50 focus-within:border-blue-300">
+                            <input
+                                onChange={(
+                                    e: React.ChangeEvent<HTMLInputElement>
+                                ) => setIngredient(e.target.value)}
+                                className="box-border p-2 tracking-wider rounded outline-0"
+                                type="text"
+                                placeholder="Ingredients"
+                                value={ingredient}
+                            />
+                            <div
+                                className="cursor-pointer h-fit"
+                                onClick={() => {
+                                    setIngredients([
+                                        ...ingredients,
+                                        ingredient,
+                                    ]);
+                                    setIngredient("");
+                                }}
                             >
-                                <circle cx="12" cy="12" r="11" />
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    viewBox="0 0 24 24"
+                                    className="hover:fill-blue-500 fill-gray-500"
+                                >
+                                    <circle cx="12" cy="12" r="11" />
 
-                                <path
-                                    fill="white"
-                                    d="M13 11V6.5h-2V11H6.5v2H11v4.5h2V13h4.5v-2z"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                    <textarea
-                        name="preparation"
-                        id="preparation"
-                        className="outline-0 rounded min-h-16 border-2 p-2 border-gray-800/50  focus:border-blue-500"
-                        placeholder="Preparartion"
-                    ></textarea>
-                    <div className="flex justify-around ">
-                        <button className="cursor-pointer px-4 py-1 rounded-lg hover:bg-cyan-600 bg-cyan-500 shadow-lg shadow-cyan-500/50 ">
-                            Prepare
-                        </button>
-                        <button
-                            className="cursor-pointer px-4 py-1 rounded-md shadow-lg hover:bg-red-600 bg-red-500 shadow-red-500/50"
-                            onClick={() => setOpenModel(false)}
-                        >
-                            Cancel
-                        </button>
-                    </div>
+                                    <path
+                                        fill="white"
+                                        d="M13 11V6.5h-2V11H6.5v2H11v4.5h2V13h4.5v-2z"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                        <textarea
+                            name="preparation"
+                            id="preparation"
+                            className="outline-0 rounded min-h-16 border-2 p-2 border-gray-800/50  focus:border-blue-300"
+                            placeholder="Preparartion"
+                            required={true}
+                        ></textarea>
+                        <div className="flex justify-around ">
+                            <button
+                                type="submit"
+                                className="cursor-pointer px-4 py-1 rounded-lg hover:bg-cyan-600 bg-cyan-500 shadow-lg shadow-cyan-500/50 "
+                            >
+                                Prepare
+                            </button>
+                            <button
+                                className="cursor-pointer px-4 py-1 rounded-md shadow-lg hover:bg-red-600 bg-red-500 shadow-red-500/50"
+                                onClick={() => setOpenModel(false)}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
                 </div>
             )}
             <button
