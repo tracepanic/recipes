8cb9e4c7ad6199c8763452448feebe55332852e1
Anuj Gupta <anujgupta4155@gmail.com>
2025-05-13 17:29:08 +0530

    fix typo error

--- frame/CategoryWindow.tsx
+++ frame/CategoryWindow.tsx
@@ -8,16 +8,6 @@ interface CategoryWindowProps {
 }
 
 export default function CategoryWindow({ children }: CategoryWindowProps) {
-    const categories = useMemo(async () => {
-        try {
-            const response = await fetch("/api/categories");
-            const data = await response.json();
-            console.log(data);
-        } catch (err) {
-            console.log(err);
-        }
-    }, []);
-
     return (
         <div className="border-10 w-7xl h-[50vh] rounded-2xl border-black m-2 flex flex-row flex-wrap bg-slate-700">
             {children}
